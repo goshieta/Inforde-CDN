5b1994394c5e1c100a301e2e56e82aa08c8d5b64
goshieta <tamerika563@gmail.com>
2023-08-27 18:07:03 +0900

    天気のfetch意外完成

--- js/weather.js
+++ js/weather.js
@@ -1,3 +1,41 @@
+import * as Vue from "https://cdn.jsdelivr.net/npm/vue@3.2/dist/vue.esm-browser.js";
+
 export const Weather = {
-  template: "<div></div>",
+  template: `<div>
+    <div id="weatherHead">
+      <p>{{pointInfo.point}}</p>
+    </div>
+      <div v-for="oneDay in pointInfo.weatherArray">
+        <div class="weatherImgArea">
+          <img src="">
+        </div>
+        <div class="weatherStringArea">
+          <p>{{oneDay.day}}</p>
+          <div class="weatherStringTD">
+            <p>気温</p>
+            <p>{{oneDay.temp}}</p>
+          </div>
+          <div class="weatherStringTD">
+            <p>降水確率</p>
+            <p>{{oneDay.forecast}}</p>
+        </div>
+        </div>
+      </div>
+  </div>`,
+  props: ["point"],
+  setup(props) {
+    const pointInfo = Vue.reactive({
+      point: "読み込み中",
+      weatherArray: [],
+      today: {
+        day: "",
+        weather: "",
+        temp: "",
+        forecast: "",
+      },
+    });
+    return {
+      pointInfo,
+    };
+  },
 };
